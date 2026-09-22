d813ffb944cd4e16ef6fcb9fa7dc971f6d8ad53b
nirayndra <faadiajasmine@gmail.com>
2026-09-22 15:57:21 +0700

    first commit

---
+++ 22Sept26/EvaluateStudent.py
@@ -0,0 +1,13 @@
+# 1 Evaluate Student
+percentage = float(input("Enter the percentage: "))
+
+if percentage >= 90:
+    print("Excellent performance!")
+elif percentage >= 80:
+    print("Very Good performance!")
+elif percentage >= 70:
+    print("Good performance!")
+elif percentage >= 60:
+    print("Average performance!")
+else:
+    print("Below Average")
